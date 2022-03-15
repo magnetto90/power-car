1271d29805eef2d9b2342b9b6bcb65ce8b8b83b2
Erik <esnetto@gmail.com>
2022-03-15 16:45:30 -0300

    collection rules out

--- src/App.vue
+++ src/App.vue
@@ -93,11 +93,9 @@
         </v-card-title>
 
         <v-card-text>
-          1. 100 Cars are going to be minted before 31/12/2021 <br>
-          2. Race Rules: If you run a race and lose, you also lose your car. But if you win, you also win your oponent´s car.<br>
-          3. Bonus: Increase the chances of wining a race. For example +5 gives a 10% more chances to win.<br>
-          4. Collection Rules: If you are the first in completing a Collection (buy 4 cars) you will be given the Special car of that collection.<br>
-          5. Fee, at the beginning, for selling a car is 5%, it will never be higher, it is written in the smart contract. It can only decrease.
+          1. 90 were minted. <br>
+          2. Bonus: Increase the chances of wining a race. Higher bonus is better.<br>
+          3. Fee, at the beginning, for selling a car is 5%, it will never be higher, it is written in the smart contract. It can only decrease.
         </v-card-text>
 
         <v-divider></v-divider>
